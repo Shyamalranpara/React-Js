420b5a187c4faaa7e8be88126c09effbd1ea4963
shyamalranpara <shyamalranpara7846@gmail.com>
2025-01-09 03:38:21 +0530

    App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import Hello from './Components/Hello';
-import Bye from './Components/Bye';
-import Counter from './Components/Counter';
-import Blinkit from './Components/Blinkit';
-import Ex from './Components/Ex';
-import Greeting from './Components/Greeting';
-import Star from './Components/Star';
 
-import Product from './Components/Product';
-import Count from './Components/Count';
-import Hooks from './Components/Hooks';
-import State from './Components/State';
-import Userinput from './Components/Userinput';
 // import {Route,Routes} from 'react-router-dom';
 // import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
-import Home from './Components/Home';
-import About from './Components/About';
-import Form from './Components/Form';
-import Log from './Components/Log';
-// import Singup from './Components2/Singup';
-// import Login from './Components2/Login';
-// import { Route, Routes } from 'react-router-dom';
-// import Products from './Components2/Products';
-
-
 
 import { useEffect, useState } from 'react';
 import { Link,Navigate, Route, Routes} from 'react-router-dom';
@@ -33,6 +10,7 @@ import Signup from './Components2/Signup';
 import Login from './Components2/Login';
 import Products from './Components2/Products';
 import Cart from './Components2/Cart';
+
 function App() {
 
   const[isLoggedin,setIsLoggedin]=useState(JSON.parse(localStorage.getItem("currentUser")) || null);
@@ -42,18 +20,6 @@ function App() {
   },[isLoggedin]);
   return (
     <div className='App'>
-
-{/* 
-      <Routes>
-
-      <Route path="/" element={<Counter a="Hello, Counter!" />} />
-
-      <Route path="/Blinkit" element={<Blinkit />} />
-
-      <Route path="/" element={<Home/>}/>
-
-      <Route path="/About" element={<About/>}/>
-      </Routes> */}
 
     <Routes>
     <Route path="/" element={<Signup/>}/>
@@ -66,47 +32,7 @@ function App() {
       </Routes>
 
       
-      {/* <Form/> */}
-
-      {/* <Log/> */}
-       
-    {/* <Home/> */}
-
-      {/* <Count/> */}
-
-      {/* <Product/> */}
-      
-      <br/>
-
-    {/* <Userinput/> */}
-
-    <br/>
-    
-      {/* <Hooks/> */}
-
-      {/* <State/> */}
-      {/* <Bye a={"Bye"} b={10}/> */}
-        
-      {/* <Hello/> */}
-
-
-
-      {/* <Blinkit /> */}
-
-
-    {/* <Star name="Shyamal" age={22} city="Rajkot,gujarat"/> */}
-
-    {/* <Count/> */}
-
-    {/* <Ex/> */}
-
-    {/* <div>
-  <Greeting name="Alice"  /> 
-  <Greeting name="Bob" />
-    </div> */}
-
-  
-    </div>
+      </div>
   );
 }
 
